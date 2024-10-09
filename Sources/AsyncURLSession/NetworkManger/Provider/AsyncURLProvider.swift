d6cf42fb78bc3c175b2c2cee44eac4f62856b4e5
Roy <shuwj81@daum.net>
2024-10-09 18:21:14 +0900

    feat  change type

--- Sources/AsyncURLSession/NetworkManger/Provider/AsyncURLProvider.swift
+++ Sources/AsyncURLSession/NetworkManger/Provider/AsyncURLProvider.swift
@@ -8,21 +8,22 @@
 import Foundation
 
 @available(iOS 13.0.0, *)
-public class AsyncURLProvider {
-    public init() {}
+public class AsyncURLProvider<T: TargetType> {
+    private let session: URLSession
 
-    public func requestAsync<T: Decodable & Sendable>(
-        _ target: TargetType,
-        decodeTo type: T.Type
-    ) async throws -> T? {
+        init(session: URLSession = .shared) {
+            self.session = session
+        }
+        
+        public func requestAsync<D: Decodable & Sendable>(_ target: T, decodeTo type: D.Type) async throws -> D? {
             let request = URLRequestBuilder.buildRequest(from: target)
-            let (data, response) = try await URLSession.shared.data(for: request)
+            let (data, response) = try await session.data(for: request)
             guard let httpResponse = response as? HTTPURLResponse else {
                 throw DataError.noData
             }
             switch httpResponse.statusCode {
             case 200, 201, 204:
-                return try data.decoded(as: T.self)
+                return try? data.decoded(as: D.self)
             case 400:
                 throw DataError.customError("Bad Request (400)")
             case 404:
@@ -31,5 +32,4 @@ public class AsyncURLProvider {
                 throw DataError.unhandledStatusCode(httpResponse.statusCode)
             }
         }
-    
 }
