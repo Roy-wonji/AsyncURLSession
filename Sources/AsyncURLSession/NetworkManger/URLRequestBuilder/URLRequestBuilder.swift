df1020c5c36b4333ccce48c96b9f25bb78f51dfd
Roy <shuwj81@daum.net>
2024-10-09 23:36:10 +0900

    ios 버전 대응

--- Sources/AsyncURLSession/NetworkManger/URLRequestBuilder/URLRequestBuilder.swift
+++ Sources/AsyncURLSession/NetworkManger/URLRequestBuilder/URLRequestBuilder.swift
@@ -8,42 +8,43 @@
 import Foundation
 
 
+// URL 요청을 생성하는 클래스
 public class URLRequestBuilder {
     public init() {}
-    
+
     static func buildRequest(from target: TargetType) -> URLRequest {
-            let url = target.baseURL.appendingPathComponent(target.path)
-            var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
-            var request = URLRequest(url: components?.url ?? url)
-            request.httpMethod = target.method.rawValue
+        let url = target.baseURL.appendingPathComponent(target.path)
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        var request = URLRequest(url: components?.url ?? url)
+        request.httpMethod = target.method.rawValue
 
-            if let headers = target.headers {
-                headers.forEach { request.setValue($1, forHTTPHeaderField: $0) }
-            }
-
-            switch target.task {
-            case .requestParameters(let parameters, let encoding):
-                do {
-                    request = try encoding.encode(request, with: parameters)
-                } catch {
-                    Log.error("Failed to encode parameters: \(error.localizedDescription)")
-                }
-            case .requestCompositeParameters(let bodyParameters, let bodyEncoding, let urlParameters):
-                do {
-                    components?.queryItems = urlParameters.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
-                    request.url = components?.url
-                    request = try bodyEncoding.encode(request, with: bodyParameters)
-                } catch {
-                    Log.error("Failed to encode composite parameters: \(error.localizedDescription)")
-                }
-            case .requestPlain:
-                
-                components?.queryItems = nil
-                request.url = components?.url
-            default:
-                break
-            }
-
-            return request
+        if let headers = target.headers {
+            headers.forEach { request.setValue($1, forHTTPHeaderField: $0) }
         }
+
+        switch target.task {
+        case .requestParameters(let parameters, let encoding):
+            do {
+                request = try encoding.encode(request, with: parameters)
+            } catch {
+                Log.error("Failed to encode parameters: %{public}@", error.localizedDescription)
+            }
+        case .requestCompositeParameters(let bodyParameters, let bodyEncoding, let urlParameters):
+            do {
+                components?.queryItems = urlParameters.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
+                request.url = components?.url
+                request = try bodyEncoding.encode(request, with: bodyParameters)
+            } catch {
+                Log.error("Failed to encode composite parameters: %{public}@", error.localizedDescription)
+            }
+        case .requestPlain:
+            // No parameters should be added for requestPlain, ignoring any accidental parameters
+            components?.queryItems = nil
+            request.url = components?.url
+        default:
+            break
+        }
+
+        return request
+    }
 }
